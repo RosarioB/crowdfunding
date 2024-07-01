aeba52f83c927334503810df496db6d6d7951f17
Rosario Borgesi <borgesiros@gmail.com>
2024-07-01 14:44:30 +0200

    refactoring App as functional component

--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -1,37 +1,41 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { CardGroup, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
 import Layout from "../components/Layout";
 
-class CampaignIndex extends Component {
-  static async getInitialProps() {
-    const campaigns = await factory.methods.getDeployedCampaigns().call();
-    return { campaigns };
-  }
+const CampaignIndex = () => {
+  const [campaigns, setCampaigns] = useState([]);
 
-  renderCampaigns() {
-    const items = this.props.campaigns.map((address) => {
+  useEffect(() => {
+    const fetchCampaigns = async () => {
+      const campaigns = await factory.methods.getDeployedCampaigns().call();
+      setCampaigns(campaigns);
+    };
+
+    fetchCampaigns();
+  }, []);
+
+  const renderCampaigns = () => {
+    const items = campaigns.map((address) => {
       return {
         header: address,
         description: <a>View Campaign</a>,
-        fluid: true
+        fluid: true,
       };
     });
 
     return <CardGroup items={items} />;
-  }
+  };
 
-  render() {
-    return (
-      <Layout>
-        <div>
-          <h3>Open Campaigns</h3>
-          <Button floated="right" content="Create Campaign" icon="add" primary />
-          {this.renderCampaigns()}
-        </div>
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout>
+      <div>
+        <h3>Open Campaigns</h3>
+        <Button floated="right" content="Create Campaign" icon="add" primary />
+        {renderCampaigns()}
+      </div>
+    </Layout>
+  );
+};
 
 export default CampaignIndex;
